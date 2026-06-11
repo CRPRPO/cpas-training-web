0c001d8ef4cbac8bbaba23326af8f81e3ce4210f
CRPRPO <s110321504@mail1.ncnu.edu.tw>
2026-06-11 12:39:41 +0800

    Update SUBMIT_ENDPOINT URL in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 // 請將 Google Apps Script 部署後取得的 Web App URL 貼在這裡。
 // URL 必須以 /exec 結尾。
-const SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycbzeW5DwXYctCa94xrhHOcExxi1Z3R26h-SexMgwvOJ4xw6kGjqfiCU97Jh5xoJqxto/exec";
+const SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycby5DbjF2-dk_mc5TMmbxX21zn4rqedOlVffTEzwtbdVA0jCq2MMuw9NsRTDX5NLLf9q7A/exec";
 
 // V8 分欄送出欄位名稱。
 // 若使用 README_V8.txt 提供的 Apps Script，可保持預設。
