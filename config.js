51373363514356cba90c3d203f7d65c5b91ce006
CRPRPO <s110321504@mail1.ncnu.edu.tw>
2026-06-13 19:57:34 +0800

    Update SUBMIT_ENDPOINT with new Google Apps Script URL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 // 請將 Google Apps Script 部署後取得的 Web App URL 貼在這裡。
 // URL 必須以 /exec 結尾。
-const SUBMIT_ENDPOINT = "";
+const SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycby5DbjF2-dk_mc5TMmbxX21zn4rqedOlVffTEzwtbdVA0jCq2MMuw9NsRTDX5NLLf9q7A/exec";
 
 const SUBMIT_FIELDS = {
   timestamp: "timestamp",
